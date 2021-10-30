1f2587c31eaac56255ee3b6145c442ceb9b2b838
bonny-412 <bonny-412@users.noreply.github.com>
2021-10-30 16:14:18 +0200

    Initial commit

---
+++ app/src/main/java/it/bonny/app/wisespender/MainActivity.java
@@ -0,0 +1,44 @@
+package it.bonny.app.wisespender;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import it.bonny.app.wisespender.fragment.HomeFragment;
+import it.bonny.app.wisespender.fragment.StatisticsFragment;
+import it.bonny.app.wisespender.fragment.TransactionFragment;
+import it.bonny.app.wisespender.fragment.UserFragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.rootLayout, new HomeFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if(item.getItemId() == R.id.home) {
+                fragment = new HomeFragment();
+            }else if(item.getItemId() == R.id.statistics) {
+                fragment = new StatisticsFragment();
+            }else if(item.getItemId() == R.id.transactions) {
+                fragment = new TransactionFragment();
+            }else if(item.getItemId() == R.id.user) {
+                fragment = new UserFragment();
+            }
+
+            if(fragment != null)
+                getSupportFragmentManager().beginTransaction().replace(R.id.rootLayout, fragment).commit();
+
+            return true;
+        });
+
+    }
+
+}
---
+++ app/src/main/java/it/bonny/app/wisespender/fragment/UserFragment.java
@@ -0,0 +1,20 @@
+package it.bonny.app.wisespender.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import it.bonny.app.wisespender.R;
+
+public class UserFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_fragment, container, false);
+    }
+
+}
